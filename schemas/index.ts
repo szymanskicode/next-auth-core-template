4322a03b724fddc520257cb209768da8a4076230
Piotr Szymański <piotrszymanskipoczta@gmail.com>
2024-04-22 16:20:59 +0200

    Create register form

--- schemas/index.ts
+++ schemas/index.ts
@@ -4,3 +4,9 @@ export const LoginSchema = z.object({
   email: z.string().min(1, { message: "Email is required" }).email(),
   password: z.string().min(1, { message: "Password is required" }),
 });
+
+export const RegisterSchema = z.object({
+  email: z.string().min(1, { message: "Email is required" }).email(),
+  password: z.string().min(6, { message: "Minimum 6 characters required" }),
+  name: z.string().min(1, { message: "Name is required" }),
+});
